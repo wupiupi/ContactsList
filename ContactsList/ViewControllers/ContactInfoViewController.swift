6e69225dee72a330b4c6ba07b445926e562059b7
Paul <pasha.makey@gmail.com>
2023-12-16 19:11:43 +0300

    🔧 chore: UI setup

--- ContactsList/ViewControllers/ContactInfoViewController.swift
+++ ContactsList/ViewControllers/ContactInfoViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 final class ContactInfoViewController: UIViewController {
 
+    @IBOutlet var phoneLabel: UILabel!
+    @IBOutlet var emailLabel: UILabel!
+    
+    var contact: Contact!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+}
 
-        // Do any additional setup after loading the view.
+private extension ContactInfoViewController {
+    func setupUI() {
+        title = "\(contact.name) \(contact.surname)"
+        phoneLabel.text = "Phone: \(contact.phone)"
+        emailLabel.text = "Email: \(contact.email)"
     }
 }
